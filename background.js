f2f4d468898254a13fcffe7768a50883228346f9
Bo Zhu <zhubo03@gmail.com>
2012-05-30 15:11:48 -0400

    a small bug fix

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var unblock_youku_lite = {};  // namespace
 unblock_youku_lite.ip_addr  = "220.181.111.";
 unblock_youku_lite.ip_addr += Math.floor(Math.random() * 254 + 1); // 1 ~ 254
-console.log('faked ip addr: ' + unblock_youku.ip_addr);
+console.log('faked ip addr: ' + unblock_youku_lite.ip_addr);
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
     // callback function
